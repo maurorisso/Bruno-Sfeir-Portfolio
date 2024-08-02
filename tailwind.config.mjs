df0bded72042f5a2174ab674d74083a2e3e460d2
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-08-02 17:12:00 +0200

    chore: Add new painting collections, update header in main file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				serif: ['Cormorant Garamond', 'sans-serif'],
+				inter: ['Inter Variable', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
