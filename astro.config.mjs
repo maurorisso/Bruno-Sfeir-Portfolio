e3b402e4edea74e56bd61705c7ba7f7f70ac996b
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-09-13 23:36:19 +0200

    chore: Add @astrojs/vercel/serverless adapter for web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), partytown()]
+  integrations: [tailwind(), react(), partytown()],
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
